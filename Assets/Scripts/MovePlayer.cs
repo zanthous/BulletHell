58f19aecbf28c10186fd2423dd9b127505b8cccc
zanthous <bcm023@gmail.com>
2019-02-09 13:04:46 -0500

    Added pooling scripts, layed out interface

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveOffset = 
+            new Vector2(Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed, 
+            Input.GetAxisRaw("Vertical") * Time.deltaTime * speed);
+
+        //SpriteRenderer a = gameObject.GetComponent<SpriteRenderer>();
+
+        transform.position += (Vector3)moveOffset;
+    }
+    
+}
