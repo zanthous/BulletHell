d068e029e2161fe8235640ed6df9be64629a49df
Erich Hart <erichhart93@gmail.com>
2019-02-09 20:41:46 -0500

    added bullet reflectability in bullet class

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,15 +6,17 @@ public class Bullet
     //angle
     private float direction;
     private float speed;
+    private bool reflectable;
     //0 or more actions
     Queue actions;
     //TODO make gameobject
     string bulletObject;
-    public Bullet(string bulletObject, float direction, float speed, Queue actions)
+    public Bullet(string bulletObject, float direction, float speed, Queue actions, bool reflectable)
     {
         this.bulletObject = bulletObject;
         this.direction = direction;
         this.speed = speed;
         this.actions = actions;
+        this.reflectable = reflectable;
     }
 }
